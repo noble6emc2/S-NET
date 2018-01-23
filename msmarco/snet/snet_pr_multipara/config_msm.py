7e457aab9c5696d162a1ee5591218f891bc92d1a
burglarhobbit <bhavyapatwa007@gmail.com>
2018-01-24 03:35:37 +0530

    updated snet passage ranking

--- msmarco/snet/snet_pr_multipara/config_msm.py
+++ msmarco/snet/snet_pr_multipara/config_msm.py
@@ -116,7 +116,7 @@ flags.DEFINE_float("init_lr", 1.0, "Initial learning rate for Adadelta")
 flags.DEFINE_float("keep_prob", 0.9, "Dropout keep prob in rnn") #0.7
 flags.DEFINE_float("ptr_keep_prob", 0.9, "Dropout keep prob for pointer network") #0.7
 flags.DEFINE_float("grad_clip", 5.0, "Global Norm gradient clipping rate")
-flags.DEFINE_integer("hidden", 75, "Hidden size") #75
+flags.DEFINE_integer("hidden", 150, "Hidden size") #75
 flags.DEFINE_integer("char_hidden", 100, "GRU dimention for char")
 flags.DEFINE_integer("patience", 3, "Patience for learning rate decay")
 flags.DEFINE_string("bd","bd","bd")
